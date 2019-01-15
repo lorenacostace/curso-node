675a07d607b2151c39e3ab8c553af3eca8aa5b8b
Lorena Costa <lorenacostace@gmail.com>
2019-01-15 22:31:54 +0100

    Ejercicio version modulo, parte ii. 25

--- version_modulo/index.js
+++ version_modulo/index.js
@@ -9,14 +9,48 @@ var path = require('path');
 
 //Funcion que lea le version de un modulo
 
-var versionModulo = function (moduleName, callback) {
+var versionModulo = function (moduleName, callBack) {
     // Calculamos la ruta al fichero
     var fichero = path.join('./node_modules', moduleName, 'package.json');
+
+    //Para leer un fichero
+    fs.readFile(fichero, 'utf8',  function (err, data) {
+
+        //Comprobamos si ha habido error en la lectura del fichero
+        if(err){
+            callBack(err);
+            return;
+        }
+
+
+        // Si no ha habido error, convertimos el contenido en un objeto
+        var packageJson = {};
+        
+        try {
+            packageJson = JSON.parse(data);
+        }
+        catch (e) {
+            callBack('error al parsear ' + fichero);
+            return;
+        }
+
+
+        // Comprobamos si tiene version
+        if(packageJson && !packageJson.version){
+            callBack('No se encuentra la etiqueta "version" en ' + fichero);
+            return;
+        }
+
+        // Llamada al callback de finalizacion
+        callBack(null, packageJson.version);
+    });
 };
 
 
 // Modulo del que queremos la info
+
 var modulo = 'chance';
+
 
 // Llamada a la funcion
 

36bbd2f263169e0b0696e2176ab12a4a34a49628
Lorena Costa <lorenacostace@gmail.com>
2019-01-18 14:01:28 +0100

    Ejercicio herencia. 33

--- prototipos.js
+++ prototipos.js
@@ -1,5 +1,6 @@
 "use strict";
 
+// Creamos un constructor persona
 function Persona(nombre) {
     this.name = nombre;
 }
@@ -14,3 +15,28 @@ Persona.prototype.saluda = function(){
 console.log(persona.name);
 
 persona.saluda();
+
+
+// ---------- HEREDANDO DE PERSONA -----------------
+
+// Creamos un constructor de agentes
+function Agente(nombre) {
+    Persona.call(this, nombre);
+    // Esto ejecuta el constructor de Persona con el this de Agente, definiendi en el this de Agente una propiedad name
+    // y asignandole el parametro recibido
+}
+
+// Le asginamos como prototipo una persona
+Agente.prototype = new Persona('soy un prototipo');
+
+var agente = new Agente('Smith');
+
+agente.saluda();
+
+// Para comprobar el prototipo
+console.log(
+    Object.getPrototypeOf(agente), // Retorna su prototipo
+    agente instanceof Agente, // para comprobar si agente es una instancia de Agente
+    agente instanceof Persona,
+    agente instanceof Object
+);

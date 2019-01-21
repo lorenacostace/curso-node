86cd0234517ac8d90f9989aca0e6957b650a0c23
Lorena Costa <lorenacostace@gmail.com>
2019-01-21 21:37:28 +0100

    Ejercicio rutas parametros y respuestas. 51 Ejercicio un middleware sencillo. 53 Ejercicio doble respuesta. 54

---
+++ express_generator/generada/routes/clients.js
@@ -0,0 +1,28 @@
+"use strict";
+
+var express = require('express');
+var router = express.Router();
+
+router.use(function (req, res, next) {
+   console.log('middleware de router clientes');
+   res.send('respuesta');
+   next();
+});
+
+// Cargamos una ruta al router. Todas las peticiones a la ruta /, van a ejecutar este middleware.
+router.get('/', function (req, res, next) {
+    res.send('respuesta a clients');
+});
+
+router.get('/:id', function (req, res, next) {
+    //console.log(req.params);
+   res.send('recibido parametro ' + req.params.id);
+});
+
+router.post('/', function (req, res, next) {
+    console.log('req.body', req.body);
+    res.json({recibido: req.body.num});
+});
+
+// Exportamos el router para que app.js lo pueda cargar. Lo que estamos haciendo es un modulo.
+module.exports = router;
